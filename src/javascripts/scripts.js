61552cde25351a2cf13ffc9c31feb99e9a88df32
Tim Hartmann <hallo@timhartmann.eu>
2019-02-07 21:42:48 +0100

    chore(*): update font-size, add popup

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -21,6 +21,26 @@ $(document).ready(function() {
     $('.js-navigation-list').removeClass('is-active');
   });
 
+  // Popup
+  $('.js-popup').magnificPopup({
+    type: 'inline',
+    preloader: false,
+    focus: '#name',
+
+    // When elemened is focused, some mobile browsers in some cases zoom in
+    // It looks not nice, so we disable it:
+    callbacks: {
+      beforeOpen: function() {
+        if($(window).width() < 700) {
+          this.st.focus = false;
+        } else {
+          this.st.focus = '#name';
+        }
+      }
+    }
+  });
+
+  // Scroll
   $('.js-anchor-scroll').anchorScroll({
     scrollSpeed: 800, // scroll speed
     offsetTop: 100, // offset for fixed top bars (defaults to 0)
