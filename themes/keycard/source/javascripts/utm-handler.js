fe7d0a8a9ab89218d1c17d79d40804a7ecc6f0f2
Felicio Mununga <felicio@users.noreply.github.com>
2024-10-29 13:09:14 +0100

    u

---
+++ themes/keycard/source/javascripts/utm-handler.js
@@ -0,0 +1,35 @@
+// Pass campaign url params to Shopify on Get Keycard CTA https://github.com/status-im/keycard-site/issues/83
+
+function storeUtmParams() {
+  const utmParams = new URLSearchParams(window.location.search)
+    .toString()
+    .match(/utm_[^=&]+=[^&]+/g)
+
+  console.log('utmParams', utmParams)
+
+  if (utmParams) {
+    sessionStorage.setItem('utmParams', JSON.stringify(utmParams))
+  }
+}
+
+function forwardUtmParams() {
+  const storedUtmParams = JSON.parse(sessionStorage.getItem('utmParams'))
+  if (!storedUtmParams) return
+
+  const keycardLinks = document.querySelectorAll(
+    'a[href^="https://get.keycard.tech/"]',
+  )
+  keycardLinks.forEach((link) => {
+    const url = new URL(link.href)
+    storedUtmParams.forEach((param) => {
+      const [key, value] = param.split('=')
+      url.searchParams.set(key, value)
+    })
+    link.href = url.toString()
+  })
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  storeUtmParams()
+  forwardUtmParams()
+})
