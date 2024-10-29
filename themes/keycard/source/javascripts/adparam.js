9549166cd74f33eeefad2f92a6d10ca603007b31
Felicio Mununga <felicio@users.noreply.github.com>
2024-10-29 12:22:32 +0100

    u

--- themes/keycard/source/javascripts/adparam.js
+++ themes/keycard/source/javascripts/adparam.js
@@ -25,7 +25,7 @@ const addAdIDToLinks = (id) => {
 }
 
 (function() {
-  /* https://github.com/status-im/keycard-site/issues/83 */
+  /* pass campaign url params to Shopify on Get Keycard CTA https://github.com/status-im/keycard-site/issues/83 */
   document.querySelectorAll('a[href="https://get.keycard.tech/"]').forEach(a=>{
     let p=location.search.match(/utm_[^=&]+=[^&]+/g);
     if(p) a.href+=(a.href.includes('?')?'&':'?')+p.join('&');
